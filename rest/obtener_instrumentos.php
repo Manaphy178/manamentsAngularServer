050c3d727f35d3d6379e83253b2219a6e7a6b377
Manaphy178 <javiergonzmu@gmail.com>
2024-12-09 00:34:55 +0100

    paginacion no funciona

--- rest/obtener_instrumentos.php
+++ rest/obtener_instrumentos.php
@@ -1,8 +1,14 @@
 <?php
-
+header('Content-Type: application/json');
 require "../librerias_php/setUp.php";
+// Obtener parámetros de paginación
+$limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 12;  // Por defecto 10 elementos por página
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;      // Por defecto página 1
+$offset = ($page - 1) * $limit;  // Calcular el offset para la consulta
+$stmt = $pdo->query("SELECT COUNT(*) FROM instrumentos");
+$total_items = $stmt->fetchColumn();
 
-$instrumentos = R::getAll("
+$stmt = $pdo->prepare("
     SELECT 
         i.id, 
         i.nombre_instrumento, 
@@ -15,6 +21,17 @@ $instrumentos = R::getAll("
         marcas AS m ON i.marca_id = m.id 
     ORDER BY 
         i.id DESC
+    LIMIT :limit OFFSET :offset
 ");
+$stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+$stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+$stmt->execute();
+$instrumentos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$total_pages = ceil($total_items / $limit);
 
-echo json_encode($instrumentos);
+// Responder con los datos y la información de paginación
+echo json_encode([
+    'instrumentos' => $instrumentos,
+    'total_pages' => $total_pages,
+    'current_page' => $page
+]);
